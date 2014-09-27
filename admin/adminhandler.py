6abc50646d7e93543724d42e2299530239ed0c66
liujie <account4liu@163.com>
2014-09-27 09:23:51 +0800

    添加前端相关

--- admin/adminhandler.py
+++ admin/adminhandler.py
@@ -1,16 +1,18 @@
+import os.path
 import tornado.ioloop
 import tornado.web
 from tornado import template
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        loader = tornado.template.Loader('views/')
-        self.write(loader.load("index.html").generate(user="liujie"))
-        # self.render('views/index.html')
+        self.render('index.html')
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-], debug=True)
+application = tornado.web.Application(
+    [(r"/", MainHandler),],
+    template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+    static_path=os.path.join(os.path.dirname(__file__), 'static'),
+    debug=True
+)
 
 if __name__ == "__main__":
     application.listen(8888)
